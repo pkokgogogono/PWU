137bd7ff79afba34ee44693bc0dfcc118c3a7157
user2 <user2@KH2>
2016-04-27 15:54:56 +0900

    1

--- PWU/src/action/login/LoginProAction.java
+++ PWU/src/action/login/LoginProAction.java
@@ -13,43 +13,29 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import action.CommandAction;
+import dao.LoginDao;
 import vo.MemberVo;
 public class LoginProAction implements CommandAction {
 	
 	public String requestPro(HttpServletRequest request, HttpServletResponse response)throws Throwable{
 		request.setCharacterEncoding("euc-kr");
-
-		request.setCharacterEncoding("euc-kr");
-		String res = "config.xml";
-		HttpSession session2=request.getSession(true);
-		InputStream is;
-		try {
-			is = Resources.getResourceAsStream(res);
-
-		SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(is);
-		SqlSession session = factory.openSession();
 		
-		if(session.selectOne("member.idcheck",request.getParameter("id"))!=null)
-		{	
+		HttpSession session2=request.getSession(true);
+		LoginDao logindao = LoginDao.getInstance();
+		
+		
+		if(logindao.idCheck(request.getParameter("id"))!=null)
+		{
 			session2.setAttribute("check",0);
 			MemberVo vo = new MemberVo(request.getParameter("id"),request.getParameter("passwd"));
-			if(((int)session.selectOne("member.logincheck", vo))==1)
+			if((logindao.loginCheck(vo))==1)
 			{
-
 				session2.setAttribute("memId",request.getParameter("id"));
 				return "/login/index.jsp";
 			}
 		}
 		else{
 			session2.setAttribute("check",1);
-		}
-		
-		session.commit();
-
-		session.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		}
 		
 		return "/login/loginPro.jsp";
